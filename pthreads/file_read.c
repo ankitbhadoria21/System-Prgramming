4c66625aa3f36c3d134ad6ecc53ea652ec0c5f00
ankitbhadoria21 <ankit.bhadoria21@gmail.com>
2016-02-06 02:48:23 -0500

    done integrating for 2 mapper threads

--- pthreads/file_read.c
+++ pthreads/file_read.c
@@ -10,11 +10,22 @@
 char mapper_pool[MAX_BUF_SIZE];
 char reducer_pool[5*MAX_BUF_SIZE];
 pthread_mutex_t mapper_lock=PTHREAD_MUTEX_INITIALIZER;
-//pthread_mutex_t reducer_lock=PTHREAD_MUTEX_INITIALIZER;
+pthread_mutex_t reducer_lock=PTHREAD_MUTEX_INITIALIZER;
 pthread_mutex_t gen_read=PTHREAD_MUTEX_INITIALIZER;
 sem_t sem_write,sem_read,sem_read1,sem_write1;
+sem_t sem_read2,sem_write2;
+int doneReading=0,doneReading1=0;
+int no_of_mapper_thread,no_of_reducer_thread,no_of_summarizer_thread;
+pthread_t mapper_thread;
+pthread_t mapper_thread1;
+pthread_t reducer_thread;
+pthread_t reducer_thread1;
+pthread_t reducer_thread2;
+pthread_t write_thread;
+pthread_t file_reader;
 
-int doneReading=0;
+extern void* wordCountWriter(void *);
+extern void* reducer(void *);
 
 void* mapper_pool_updater(void *buf)
 {
@@ -44,6 +55,8 @@ fclose(fs);
 sem_wait(&sem_read);
 doneReading=1;
 sem_post(&sem_write);
+sem_post(&sem_write);
+printf("Mapper Updater Thread %lld exiting\n",pthread_self());
 }
 
 
@@ -52,19 +65,27 @@ void* mapper(void *map)
 char buffer[MAX_BUF];
 int done;
 while(1){
+printf("Mapper Thread %lld started\n",pthread_self());
 int i=0,i1=0;
 sem_wait(&sem_write);
 pthread_mutex_lock(&gen_read);
 done=doneReading;
-pthread_mutex_unlock(&gen_read);
 if(done){
+doneReading1=1;
 sem_post(&sem_read);
-break;
+sem_post(&sem_write1);
+pthread_mutex_unlock(&gen_read);
+printf("Mapper Thread %lld exiting\n",pthread_self());
+pthread_exit(0);
 }
-
+else {
+pthread_mutex_unlock(&gen_read);
+}
+sem_wait(&sem_read1);
 pthread_mutex_lock(&mapper_lock);
 while(mapper_pool[i]) {
-buffer[i1++]=mapper_pool[i++];
+buffer[i1++]=mapper_pool[i];
+mapper_pool[i++]=0;
 }
 
 char *pointToCh;int size=0;
@@ -75,31 +96,51 @@ while(pointToCh!=NULL)
 sprintf(reducer_pool+size,"(%s,1) ",pointToCh);
 size+=strlen(pointToCh)+5;
 pointToCh=(char*)strtok(NULL," \n");
-puts(reducer_pool);
+//puts(reducer_pool);
 }
 pthread_mutex_unlock(&reducer_lock);
 pthread_mutex_unlock(&mapper_lock);
+sem_post(&sem_write1);
 sem_post(&sem_read);
+memset(buffer,0,MAX_BUF);
+}
+peintf("Mapper Thread %lld Exited\n");
 }
 
-}
-
-
-
-
-int main(){
+void init() {
 sem_init(&sem_write,0,0);
 sem_init(&sem_read,0,1);
 sem_init(&sem_read1,0,1);
 sem_init(&sem_write1,0,0);
+sem_init(&sem_write2,0,0);
+sem_init(&sem_read2,0,1);
+}
+
+
+int main(int argc,char* argv[]){
+init();
+//no_of_mapper_thread=itoa(argv[1]);
+//no_of_reducer_thread=itoa(argv[2]);
+/*
 pthread_t mapper_thread;
 pthread_t mapper_thread1;
+pthread_t reducer_thread;
+pthread_t reducer_thread1;
+pthread_t reducer_thread2;
+pthread_t write_thread;
 pthread_t file_reader;
+*/
 char buf[]="./a.txt";
 pthread_create(&file_reader,NULL,mapper_pool_updater,(void*)buf);
 pthread_create(&mapper_thread,NULL,mapper,NULL);
 pthread_create(&mapper_thread1,NULL,mapper,NULL);
+pthread_create(&reducer_thread,NULL,reducer,NULL);
+pthread_create(&write_thread,NULL,wordCountWriter,NULL);
+
 pthread_join(file_reader,NULL);
 pthread_join(mapper_thread,NULL);
+pthread_join(mapper_thread1,NULL);
+pthread_join(reducer_thread,NULL);
+pthread_join(write_thread,NULL);
 return 0;
 }
--- pthreads/red_summ.c
+++ pthreads/red_summ.c
@@ -1,3 +1,4 @@
+
 #include<stdio.h>
 #include<string.h>
 #include<stdlib.h>
@@ -7,11 +8,22 @@
 #define SENTINAL '%'
 #define MAX_BUF 256
 
-char reducer_pool[5*MAX_BUF]="(fsjka,1) (fsjka,1) (fsjka,1) (fsjka,1) (fsjka,1) (fhsjdkhda,1) (jkdka,1) (jkdka,1) (hjfhsjf,1) (hjfhsjf,1) (hjfhsjf,1) (hajfhaj,1) (hajfhaj,1) (hajfhaj,1) (hajfhaj,1) (fsjka,1) (faj,1)";
-pthread_mutex_t reducer_lock=PTHREAD_MUTEX_INITIALIZER;
+extern char reducer_pool[5*MAX_BUF];//="(fsjka,1) (fsjka,1) (fsjka,1) (fsjka,1) (fsjka,1) (fhsjdkhda,1) (jkdka,1) (jkdka,1) (hjfhsjf,1) (hjfhsjf,1) (hjfhsjf,1) (hajfhaj,1) (hajfhaj,1) (hajfhaj,1) (hajfhaj,1) (fsjka,1) (faj,1)";
+extern pthread_mutex_t reducer_lock;
 char summarizer_pool[5*MAX_BUF];
 pthread_mutex_t summarizer_lock=PTHREAD_MUTEX_INITIALIZER;
 sem_t sem_read2,sem_write2;
+extern sem_t sem_read1,sem_write1;
+extern int doneReading;
+extern pthread_mutex_t gen_read;
+extern int no_of_mapper_thread,no_of_reducer_thread,no_of_summarizer_thread;
+extern pthread_t mapper_thread;
+extern pthread_t mapper_thread1;
+extern pthread_t reducer_thread;
+extern pthread_t reducer_thread1;
+extern pthread_t reducer_thread2;
+extern pthread_t write_thread;
+extern pthread_t file_reader;
 
 typedef struct node{
 char str[MAX_BUF];
@@ -21,20 +33,41 @@ struct node *next;
 
 pthread_mutex_t node_lock=PTHREAD_MUTEX_INITIALIZER;
 node node_list[256]={0};
+int  doneReading2=0;
+extern int doneReading1;
 
-void reducer()
+void* reducer(void *a)
 {
 char buffer[5*MAX_BUF];
 char *str,prev_char=SENTINAL;
 char *tmp,*word_list[MAX_BUF];
-
+int done;
+while(1) {
+sem_wait(&sem_write1);
+pthread_mutex_lock(&gen_read);
+done=doneReading1;
+if(done){
+doneReading2=1;
+sem_post(&sem_read1);
+sem_post(&sem_write2);
+pthread_mutex_unlock(&gen_read);
+printf("Reducer Thread %lld Exiting\n",pthread_self());
+pthread_exit(0);
+}
+else {
+pthread_mutex_unlock(&gen_read);
+}
+//printf("before\n");
 sem_wait(&sem_read2);
+//printf("After\n");
 prev_char=SENTINAL;
 int i=0,i1=0,index=0,ttl_wrd=0,size=0;
 
 pthread_mutex_lock(&reducer_lock);
 while(reducer_pool[i1]) {
-buffer[i++]=reducer_pool[i1++];
+//puts(reducer_pool);
+buffer[i++]=reducer_pool[i1];
+reducer_pool[i1++]=0;
 }
 pthread_mutex_unlock(&reducer_lock);
 
@@ -46,8 +79,10 @@ tmp=strtok(NULL," ");
 
 pthread_mutex_lock(&node_lock);
 while(index<ttl_wrd){
+
 str=strtok(word_list[index++],",");
 str+=1;
+
 if(prev_char!=SENTINAL && str[0]!=prev_char){
 node *tmp=&node_list[prev_char];
 node *prev_tmp=tmp;
@@ -66,13 +101,15 @@ printf("\n");
 size+=strlen(tmp->str)+5;
 tmp=tmp->next;
 prev_tmp->next=NULL;
-free(prev_tmp);
+
+//free(prev_tmp);
 }
 pthread_mutex_unlock(&summarizer_lock);
 }
 
 node *tmp=&node_list[str[0]];
-node *prev_tmp;
+node *prev_tmp=tmp;
+tmp=tmp->next;
 
 while(tmp){
 if(!strcmp(tmp->str,str))
@@ -96,43 +133,83 @@ prev_char=str[0];
 }
 
 if(prev_char!=SENTINAL){
-//size=0;
+printf("%c\n",prev_char);
+
 node *tmp=&node_list[prev_char];
+node *prev_tmp=tmp;
 tmp=tmp->next;
-node *prev_tmp;
+prev_tmp->next=NULL;
 pthread_mutex_lock(&summarizer_lock);
 while(tmp){
 sprintf(summarizer_pool+size,"(%s,%d)\n",tmp->str,tmp->count);
 //puts(summarizer_pool+size);
+int i;
+for(i=0;summarizer_pool[i];++i)
+printf("%c",summarizer_pool[i]);
+printf("\n");
+
 size+=strlen(tmp->str)+5;
 prev_tmp=tmp;
 tmp=tmp->next;
-free(prev_tmp);
+prev_tmp->next=NULL;
+//free(prev_tmp);
 }
 pthread_mutex_unlock(&summarizer_lock);
 }
+
 pthread_mutex_unlock(&node_lock);
 sem_post(&sem_write2);
+sem_post(&sem_read1);
+memset(buffer,0,5*MAX_BUF);
+}
+return NULL;
 }
 
-
-void wordCountWriter(){
+void* wordCountWriter(void *a){
+int done;
+while(1) {
 sem_wait(&sem_write2);
+//printf("in writer\n");
+pthread_mutex_lock(&gen_read);
+done=doneReading2;
+//printf("%d\n",doneReading2);
+if(done){
+pthread_mutex_unlock(&gen_read);
 FILE* fp=fopen("./wordCount.txt","a+");
 pthread_mutex_lock(&summarizer_lock);
 int i=0;
+//printf("in\n");
 while(summarizer_pool[i]) {
-fputc(summarizer_pool[i++],fp);
+fputc(summarizer_pool[i],fp);
+printf("%c",summarizer_pool[i]);
+summarizer_pool[i]='\0';
+i++;
 }
 pthread_mutex_unlock(&summarizer_lock);
 fclose(fp);
 sem_post(&sem_read2);
+printf("Writer Thread %lld exiting\n",pthread_self());
+pthread_exit(0);
 }
+else {
+pthread_mutex_unlock(&gen_read);
+}
+//printf("in writer1\n");
 
-int main()
-{
-sem_init(&sem_write2,0,0);
-sem_init(&sem_read2,0,1);
-reducer();
-wordCountWriter();
+FILE* fp=fopen("./wordCount.txt","a+");
+pthread_mutex_lock(&summarizer_lock);
+int i=0;
+//printf("write\n");
+while(summarizer_pool[i]) {
+fputc(summarizer_pool[i],fp);
+summarizer_pool[i]='\0';
+i++;
+}
+pthread_mutex_unlock(&summarizer_lock);
+fclose(fp);
+//printf("inc\n");
+sem_post(&sem_read2);
+//printf("done\n");
+}
+return NULL;
 }
