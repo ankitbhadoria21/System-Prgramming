97e9eb8240a6a471bc2c6d884b009fdfe6a4581d
ankitbhadoria21 <ankit.bhadoria21@gmail.com>
2016-03-02 09:42:44 -0500

    added direction and buffer copyin

--- thrd_safe_chr_driver/drv.c
+++ thrd_safe_chr_driver/drv.c
@@ -22,13 +22,20 @@ int devNo;
 static struct asp_mycdrv cdrv;
 static int drv_num=-1;
 static char *ramdisk=NULL;
+static char *temp=NULL;
 static ssize_t read_my(struct file *, char *, size_t, loff_t *);
 static ssize_t my_write(struct file *, char *, size_t, loff_t *);
 static long my_ioctl(struct file *, unsigned int, unsigned long);
-struct file_operations fops={.unlocked_ioctl=my_ioctl,.read=read_my,.write=my_write};
+static loff_t my_llseek(struct file *filp, loff_t off, int whence);
+struct file_operations fops={.unlocked_ioctl=my_ioctl,.read=read_my,.llseek=my_llseek,.write=my_write};
 struct class *cl;
-int noOfDevices=3;
-int direction=0;
+static int noOfDevices=3;
+static int direction=0;
+static int count=10;
+static loff_t offset=0;
+
+module_param(noOfDevices,int,S_IRUSR | S_IWUSR);
+module_param(count,int,S_IRUSR | S_IWUSR);
 
 //initializing function module_init not required with this function
 // had it been a different function module_init would be required
@@ -40,6 +47,14 @@ int init_module(void)
 	cdrv.devNo=drv_num;
 	if(drv_num!=-1){
 	ramdisk=kmalloc(SIZE,GFP_KERNEL);
+	if(!ramdisk) {
+	printk(KERN_DEBUG"Memory Allocation Failed\n");
+	return -1;
+	}
+	if(!temp) {
+	printk(KERN_DEBUG"Memory Allocation Failed\n");
+	return -1;
+	}
 	cl=class_create(THIS_MODULE,"chrdrv");
 	register_chrdev_region(drv_num,noOfDevices,"Sample");
 	for(i=0;i<noOfDevices;++i)
@@ -56,6 +71,7 @@ int init_module(void)
 void cleanup_module(void) 
 {
 	kfree(ramdisk);
+	kfree(temp);
 	cdev_del(&(cdrv.cd));
 	device_destroy(cl,drv_num);
 	class_destroy(cl);
@@ -66,33 +82,28 @@ void cleanup_module(void)
 
 loff_t my_llseek(struct file *filp, loff_t off, int whence)
 {
-    loff_t newpos;
-    switch(whence) {
-      case 0: /* SEEK_SET */
-        newpos = off;
-        break;
-
-      case 1: /* SEEK_CUR */
-        newpos = filp->f_pos + off;
-        break;
-/*
-      case 2: 
-        newpos = dev->size + off;
-        break;
-*/
-      default: 
-        return -EINVAL;
-    }
-    if (newpos < 0) return -EINVAL;
-    filp->f_pos = newpos;
-    return newpos;
+return 0;
 }
 
 static ssize_t read_my(struct file *a, char *b, size_t c, loff_t *d)
 {
-	char str[MAX_LENGTH];
-	strcpy(str,"IOCTL Read Done\n");
-	copy_to_user(b,str,MAX_LENGTH);
+	int i;
+	if(!direction) {
+	for(i=0;i<count && *d+i < SIZE;++i) {
+	temp[i]=ramdisk[*d+i];
+	}
+	temp[i]='\0';
+	copy_to_user(b,temp,c);
+	}
+	else {
+	int j;
+	for(i=count,j=0;i>=0 && *d-i >= 0;--i,j++) {
+        temp[j]=ramdisk[*d-i];
+        }
+        temp[j]='\0';
+        copy_to_user(b,temp,c);
+	}
+	*d=0;
 	return 0;
 }
 
@@ -101,6 +112,7 @@ static ssize_t my_write(struct file *a, char *b, size_t c, loff_t *d)
 	char str[MAX_LENGTH];
 	copy_from_user(str,b,c);
 	printk(KERN_INFO"Message Received from UserSpace: %s\n",str);
+	*d=0;
 	return 0;
 }
 
@@ -108,10 +120,10 @@ static long my_ioctl(struct file *b, unsigned int num, unsigned long param)
 {
 	switch(num) {
 	case MY_READ:
-	read_my(b,(char*)param,sizeof((char*)param),0);
+	read_my(b,(char*)param,MAX_LENGTH,&offset);
 	break;
 	case MY_WRITE:
-	my_write(b,(char*)param,MAX_LENGTH,0);
+	my_write(b,(char*)param,MAX_LENGTH,&offset);
 	break;
 	case ASP_CHGACCDIR:
 	down(&cdrv.sem);
