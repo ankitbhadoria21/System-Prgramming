89a863fc77937fc321a30c9a323678d4f33cf67b
ankitbhadoria21 <ankit.bhadoria21@gmail.com>
2016-03-14 16:01:49 -0400

    updated sample Application

--- thrd_safe_chr_driver/sample.c
+++ thrd_safe_chr_driver/sample.c
@@ -3,15 +3,70 @@
 #include<sys/ioctl.h>
 #include<fcntl.h>
 #include<string.h>
+#define MAX 100
+
 int main() {
-char buf[20]="reverse";
-char buf1[20]="this is buffer";
-int id=open("/dev/mycdrv2",O_RDWR);
-//buf will receive the old direction of writing in the driver
-ioctl(id,ASP_CHGACCDIR,buf);
-write(id,buf1,14);
-id=open("/dev/mycdrv0",O_RDWR);
-write(id,buf,5);
-close(id);
+int i;
+char dev_name[MAX],buf[50*MAX],read_buf[50*MAX];
+char dir[MAX];
+int id[MAX];
+int ind=0,opt,opr,direction,whence,to_read;
+long long off;
+while(1) {
+printf("Enter the name of the device to open or type \"skip\" to skip this step or \"quit\" to quit\n");
+scanf("%s",dev_name);
+if(!strcmp(dev_name,"quit")) break;
+if(strcmp(dev_name,"skip")) {
+printf("opening %s\n",dev_name);
+id[ind++]=open(dev_name,O_RDWR);
+if(id[ind-1]==-1) {
+printf("device not opened\n");
+ind--;
+continue;
+}
+}
+printf("Enter Device number to use, devices till now have number from 0 to %d\n",ind-1);
+if(ind==0) continue;
+scanf("%d",&opt);
+printf("0: Do lseek\n1: Do write\n2: Do Read\n3: Reverse Direction\n");
+scanf("%d",&opr);
+switch(opr) {
+case 0:
+printf("Enter offset to lseek to\n");
+scanf("%lld",&off);
+printf("Enter the location from which lseek needs to be done.\n0:SEEK_SET\n1:SEEK_CUR\n2:SEEK_END\n");
+scanf("%d",&whence);
+lseek(id[opt],off,whence);
+break;
+case 1:
+printf("Input Data to write\n");
+scanf("%s",buf);
+write(id[opt],buf,sizeof(buf));
+break;
+case 2:
+printf("Enter no of bytes to read\n");
+scanf("%d",&to_read);
+read(id[opt],read_buf,to_read);
+printf("Data Read is %s\n", read_buf);
+break;
+case 3:
+printf("Input\n0:for reverse direction\n1:for regular direction\n");
+scanf("%d",&direction);
+if(direction==0) {
+strcpy(dir,"reverse");
+ioctl(id[opt],ASP_CHGACCDIR,dir);
+printf("old direction is %s\n",dir);
+}
+else {
+strcpy(dir,"regular");
+ioctl(id[opt],ASP_CHGACCDIR,dir);
+printf("old direction is %s\n",dir);
+}
+break;
+}
+
+}
+for(i=0;i<ind;++i)
+close(id[ind]);
 return 0;
 }
